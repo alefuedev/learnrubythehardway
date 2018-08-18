bf3a5f00895df6cec2d73f145d5ea1d0d3e90a8e
sr.monako <don.monakodev@gmail.com>
2018-08-18 15:02:32 -0400

    exercise 1

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun"
+puts "Yay! Pritnting"
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+#If we use a hash before a sentece the command is ignore.
