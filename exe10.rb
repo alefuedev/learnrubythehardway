961263e6d8c0490500a3a7c9c223469c630e9e15
ale <aubanust@gmail.com>
2018-08-22 11:55:02 -0400

    exercise 10:What Was That? Use of new line tab and backslash \

---
+++ exe10.rb
@@ -0,0 +1,19 @@
+tabby_cat = "\tI'm tabbed in."
+# This work like the tabkey
+persian_cat = "I'm split\non a line."
+# \n Work for a new line.
+backslash_cat = "I'm \\ a \\ cat."
+#\\ Work  doing the \ in the line.
+
+fat_cat = """
+I'll do a list:
+\t*Cat food
+\t*Fishies
+\t*Catnip\n\t*Grass
+"""
+# Combnation of tabs and new line.
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
