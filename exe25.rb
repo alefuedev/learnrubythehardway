6314bb5e7b7a6feae8e051962a2f1055e825d84f
ale <aubanust@gmail.com>
2018-08-28 14:13:08 -0400

    exercise 25: Even More Practice: How to use a module, how to use it irb.

---
+++ exe25.rb
@@ -0,0 +1,51 @@
+# With this module we create our functions and the we can use them in irb.each do |member|
+# We can change the name of the module if we want to.
+# If we want to be able to use it in irb we need to require the file
+# require "./exe25.rb"
+
+module Ex25
+
+  # This function will break up words for us.
+  def Ex25.break_words(stuff)
+    words = stuff.split(' ')
+    return words
+  end
+
+  # Sorts the words.
+  def Ex25.sort_words(words)
+    return words.sort
+  end
+
+  # Prints the first word after shifting it off.
+  def Ex25.print_first_word(words)
+    word = words.shift
+    puts word
+  end
+
+  # Prints the last word after popping it off.
+  def Ex25.print_last_word(words)
+    word = words.pop
+    puts word
+  end
+
+  # Takes in a full sentence and returns the sorted words.
+  def Ex25.sort_sentence(sentence)
+    words = Ex25.break_words(sentence)
+    return Ex25.sort_words(words)
+  end
+
+  # Prints the first and last words of the sentence.
+  def Ex25.print_first_and_last(sentence)
+    words = Ex25.break_words(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
+  end
+
+  # Sorts the words then prints the first and last one.
+  def Ex25.print_first_and_last_sorted(sentence)
+    words = Ex25.sort_sentence(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
+  end
+
+end
