c73b29163c3478f155bb8659cac2287c3d0f61c5
alefuedev <alefuentesdev@gmail.com>
2018-08-29 12:27:40 -0700

    exercise 34: Accessing Elements of Arrays, how to acces elements from an array using the index starting with 0

---
+++ exe34.rb
@@ -0,0 +1,27 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#The animal at 1.
+#The third (3rd) animal.
+#The first (1st) animal.
+#The animal at 3.
+#The fifth (5th) animal.
+#The animal at 2.
+#The sixth (6th) animal.
+#The animal at 4.
+
+#ruby
+p animals[1]
+#peackock
+p animals[2]
+#bear
+p animals[0]
+#kangaroo
+p animals[3]
+#whale
+p animals[4]
+#peackock
+p animals[2]
+#playtupus
+p animals[5]
+#whale
+p animals[4]
